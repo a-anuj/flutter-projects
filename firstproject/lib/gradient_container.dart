c3e1cfe7931d4301ec8e8a9cfe2f82da5fea826b
a-anuj <anujamirthalingam5@gmail.com>
2025-07-12 21:20:17 +0530

    add a text button

--- firstproject/lib/gradient_container.dart
+++ firstproject/lib/gradient_container.dart
@@ -4,6 +4,11 @@ import "package:firstproject/styled_text.dart";
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key,required this.colors});
   final List<Color> colors;
+  
+  void rollDice(){
+    
+  }
+  
   @override
   Widget build(context){
     return Container(
@@ -15,10 +20,24 @@ class GradientContainer extends StatelessWidget {
           ),
         ),
         child :Center(
-          child: Image.asset("assets/images/dice-6.png",
-            width:200,
-          ),
-        )
+          child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+              Image.asset("assets/images/dice-6.png",
+              width:200,
+                 ),
+                TextButton(
+                      onPressed: rollDice,
+                      style:TextButton.styleFrom(
+                        padding: EdgeInsets.only(top:30,left: 30,right: 30,bottom: 30),
+                      foregroundColor: Colors.black,
+                      textStyle: const TextStyle(fontSize: 25),
+
+                    ),
+                    child: Text("Roll Dice")
+                )
+          ]),
+        ),
     );
   }
 }
