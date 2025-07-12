febcc2203b7f9b98f7c6c233d7f89b959e771192
a-anuj <anujamirthalingam5@gmail.com>
2025-07-12 08:35:23 +0530

    create a variable for colors

--- firstproject/lib/gradient_container.dart
+++ firstproject/lib/gradient_container.dart
@@ -2,15 +2,14 @@ import "package:flutter/material.dart";
 import "package:firstproject/styled_text.dart";
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({super.key,required this.colors});
+  final List<Color> colors;
   @override
   Widget build(context){
     return Container(
         decoration: BoxDecoration(
-          gradient: LinearGradient(colors:
-          [
-            Colors.purple,Colors.deepPurple
-          ],
+          gradient: LinearGradient(
+              colors:colors,
               begin: Alignment.topLeft,
               end: Alignment.bottomRight
           ),
