f90245202d4197539cdd0f98751303f061ae291d
a-anuj <anujamirthalingam5@gmail.com>
2025-07-16 18:43:54 +0530

    add the stateful widget

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/backgroundContainer.dart';
 
 class Quiz extends StatefulWidget{
+  const Quiz({super.key});
 
+  @override
+  State<Quiz> createState(){
+    return _QuizState();
+  }
 }
 
 
 class _QuizState extends State<Quiz>{
+  @override
   Widget build(context){
     return MaterialApp(
       home:Scaffold(
