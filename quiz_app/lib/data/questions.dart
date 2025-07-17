052c1518011e5bef200e820910b4691b71dd7791
a-anuj <anujamirthalingam5@gmail.com>
2025-07-17 19:16:14 +0530

    store all the questiona

---
+++ quiz_app/lib/data/questions.dart
@@ -0,0 +1,93 @@
+import 'package:quiz_app/models/quiz_questions.dart';
+
+const questions = [
+  QuizQuestion("Who is the current President of America?",
+    [
+      'Donald Trump',
+      'Barack Obama',
+      'George Washington',
+      'Joe Biden'
+    ],
+  ),
+
+  QuizQuestion("Who is the current Prime Minister of India?",
+    [
+      'Rahul Gandhi',
+      'Manmohan Singh',
+      'Indira Gandhi',
+      'Narendra Modi'
+    ],
+  ),
+
+  QuizQuestion("Who is the current Chancellor of Germany?",
+    [
+      'Angela Merkel',
+      'Olaf Scholz',
+      'Frank-Walter Steinmeier',
+      'Gerhard Schröder'
+    ],
+  ),
+
+  QuizQuestion("Who is the current President of France?",
+    [
+      'François Hollande',
+      'Nicolas Sarkozy',
+      'Jacques Chirac',
+      'Emmanuel Macron'
+    ],
+  ),
+
+  QuizQuestion("Who is the current Prime Minister of the UK?",
+    [
+      'Boris Johnson',
+      'David Cameron',
+      'Theresa May',
+      'Rishi Sunak'
+    ],
+  ),
+
+  QuizQuestion("Who is the current President of Russia?",
+    [
+      'Dmitry Medvedev',
+      'Nikita Khrushchev',
+      'Leonid Brezhnev',
+      'Vladimir Putin'
+    ],
+  ),
+
+  QuizQuestion("Who is the current President of China?",
+    [
+      'Hu Jintao',
+      'Deng Xiaoping',
+      'Mao Zedong',
+      'Xi Jinping'
+    ],
+  ),
+
+  QuizQuestion("Who is the current Prime Minister of Canada?",
+    [
+      'Stephen Harper',
+      'Pierre Trudeau',
+      'Jean Chrétien',
+      'Justin Trudeau'
+    ],
+  ),
+
+  QuizQuestion("Who is the current President of Brazil?",
+    [
+      'Jair Bolsonaro',
+      'Michel Temer',
+      'Dilma Rousseff',
+      'Luiz Inácio Lula da Silva'
+    ],
+  ),
+
+  QuizQuestion("Who is the current Prime Minister of Japan?",
+    [
+      'Yoshihide Suga',
+      'Shinzo Abe',
+      'Junichiro Koizumi',
+      'Fumio Kishida'
+    ],
+  ),
+];
