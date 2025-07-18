4f8d812cbf5d2ee675b2233c64c0e1ecac2dc2fc
a-anuj <anujamirthalingam5@gmail.com>
2025-07-18 08:03:26 +0530

    add styles and questions part

--- quiz_app/lib/question_screen.dart
+++ quiz_app/lib/question_screen.dart
@@ -13,11 +13,27 @@ class _QuestionsScreenState extends State<QuestionsScreen>{
   @override
   Widget build(context){
     return(
-      Scaffold(
-        body: Center(
-          child: Text("Hello"),
-        ),
-      )
+        SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("The Question goes here...",style: TextStyle(color: Colors.white),),
+              ElevatedButton(
+                  onPressed: (){},
+                  child: Text("Answer 1")
+              ),
+              ElevatedButton(
+                  onPressed: (){},
+                  child: Text("Answer 2")
+              ),
+              ElevatedButton(
+                  onPressed: (){},
+                  child: Text("Answer 3")
+              ),
+            ],
+          ),
+        )
     );
   }
 }
