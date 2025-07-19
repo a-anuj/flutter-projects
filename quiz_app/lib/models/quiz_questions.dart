3c8a54738567cee44ff5053ebea4d7e65f3faac2
a-anuj <anujamirthalingam5@gmail.com>
2025-07-19 12:30:23 +0530

    created a function to shuffle the temporary list not the original one

--- quiz_app/lib/models/quiz_questions.dart
+++ quiz_app/lib/models/quiz_questions.dart
@@ -3,4 +3,10 @@ class QuizQuestion{
 
   final String text;
   final List<String> answers;
+
+  List<String> shuffledAnswers(){
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
