53369c7aff7cfe3457754c32443c0a642b66da70
a-anuj <anujamirthalingam5@gmail.com>
2025-07-23 18:56:38 +0530

    add a new dart file to display the quiz summary

---
+++ quiz_app/lib/questions_summary.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class QuestionsScreen extends StatelessWidget{
+  const QuestionsScreen(this.summaryData,{super.key});
+  final List<Map<String,Object>> summaryData;
+
+  @override
+  Widget build(context) {
+    return Column();
+  }
+
+}
