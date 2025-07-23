968881676b3f88840e249a5d841073f32e8fb362
a-anuj <anujamirthalingam5@gmail.com>
2025-07-23 08:22:36 +0530

    add the results screen page

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/backgroundContainer.dart';
 import 'package:quiz_app/question_screen.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/resutls_screen.dart';
 
 class Quiz extends StatefulWidget{
   const Quiz({super.key});
@@ -26,23 +27,31 @@ class _QuizState extends State<Quiz>{
   void chooseAnswer(String answer){
     selectedAnswers.add(answer);
     setState((){
-
       if(selectedAnswers.length==questions.length){
         selectedAnswers = [];
-        activeScreen="bg-container";
+        activeScreen="results-screen";
       }
     });
   }
 
   @override
   Widget build(context){
+    Widget screenWidget = bgContainer(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen();
+    }
     return MaterialApp(
       home:Scaffold(
           body: Container(
               color: Colors.deepOrange,
-              child: activeScreen == "bg-container"
-                  ? bgContainer(switchScreen)
-                  : QuestionsScreen(onSelectAnswer: chooseAnswer),
+              child: screenWidget,
           )
       ),
     );
