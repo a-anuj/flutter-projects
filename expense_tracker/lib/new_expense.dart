b0fbe4c8f2e9d6343bedfefbe422ca8927403490
a-anuj <anujamirthalingam5@gmail.com>
2025-07-26 17:33:57 +0530

    Add functionality to the category dropdown

--- expense_tracker/lib/new_expense.dart
+++ expense_tracker/lib/new_expense.dart
@@ -15,6 +15,7 @@ class _NewExpenseState extends State<NewExpense>{
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.work;
 
 
   void _presentDatePicker() async {
@@ -81,9 +82,29 @@ class _NewExpenseState extends State<NewExpense>{
               )
               ]
             ),
+            const SizedBox(height: 16),
             Row(
               children: [
-                ElevatedButton(
+                DropdownButton(
+                  value: _selectedCategory,
+                    items: Category.values
+                        .map(
+                            (category) => DropdownMenuItem(
+                              value: category,
+                              child: Text(category.name.toUpperCase()),
+                            )
+                    ).toList(),
+                    onChanged: (value) {
+                        if(value==null){
+                          return ;
+                        }
+                        setState(() {
+                          _selectedCategory = value;
+                        });
+                    }
+                ),
+                const Spacer(),
+                TextButton(
                     onPressed: (){
                       Navigator.pop(context);
                     },
