34b3c82c859a9b482a3fa0b3d6bd8bf51242da96
a-anuj <anujamirthalingam5@gmail.com>
2025-07-26 16:48:21 +0530

    add functionality to the close button

--- expense_tracker/lib/new_expense.dart
+++ expense_tracker/lib/new_expense.dart
@@ -12,11 +12,16 @@ class NewExpense extends StatefulWidget{
 
 class _NewExpenseState extends State<NewExpense>{
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -31,14 +36,30 @@ class _NewExpenseState extends State<NewExpense>{
                   label: Text("Title")
               ),
             ),
+            TextField(
+              controller: _amountController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                prefixText: "₹ ",
+                  label: Text("Amount")
+              ),
+            ),
             Row(
               children: [
                 ElevatedButton(
                     onPressed: (){
+                      Navigator.pop(context);
+                    },
+                    child: Text("Close")
+                ),
+                ElevatedButton(
+                    onPressed: (){
                       print(_titleController.text);
+                      print(_amountController.text);
                     },
                     child: Text("Save Expense")
-                )
+                ),
+
               ],
             )
           ],
