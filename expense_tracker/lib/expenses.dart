9171e1f47c2556877545128f9d4e8302750b385f
a-anuj <anujamirthalingam5@gmail.com>
2025-07-26 22:01:17 +0530

    add the expense and display it

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -32,9 +32,16 @@ class _ExpensesState extends State<Expenses>{
   void _openAddExpenseOverlay(){
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => NewExpense(),
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
   }
+
+  void _addExpense(Expense expense){
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
 
   @override
   Widget build(context) {
