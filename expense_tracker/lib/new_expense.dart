9cdcdbaee2c96d5c9f883d8a876008e6684d1aa3
a-anuj <anujamirthalingam5@gmail.com>
2025-07-26 17:15:13 +0530

    use async-await to get the picked date and display it

--- expense_tracker/lib/new_expense.dart
+++ expense_tracker/lib/new_expense.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget{
   const NewExpense({super.key});
@@ -13,12 +14,21 @@ class NewExpense extends StatefulWidget{
 class _NewExpenseState extends State<NewExpense>{
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
 
-  void _presentDatePicker(){
+
+  void _presentDatePicker() async {
     final now = DateTime.now();
     final firstDate = DateTime(now.year-1,now.month,now.day);
-
-    showDatePicker(context: context,initialDate:now , firstDate: firstDate, lastDate: now);
+    final _pickedDate = await showDatePicker(
+        context: context,
+        initialDate:now ,
+        firstDate: firstDate,
+        lastDate: now
+    );
+    setState(() {
+      _selectedDate = _pickedDate;
+    });
   }
   
   @override
@@ -61,7 +71,7 @@ class _NewExpenseState extends State<NewExpense>{
                     mainAxisAlignment: MainAxisAlignment.end,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      const Text("Date Selected..."),
+                      Text(_selectedDate == null ? "No Date Selected" : formatter.format(_selectedDate!)),
                       IconButton(
                           onPressed: _presentDatePicker,
                           icon: Icon(Icons.calendar_month)
