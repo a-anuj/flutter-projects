93a311315a9f4b2e2cccca904466ea5b2a8f96fb
a-anuj <anujamirthalingam5@gmail.com>
2025-07-27 16:34:45 +0530

    add bgcolor to the dismissible widget

--- expense_tracker/lib/expense_item.dart
+++ expense_tracker/lib/expense_item.dart
@@ -16,8 +16,13 @@ class ExpenseItem extends StatelessWidget{
           vertical: 16,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(expense.title),
+            Text(
+                expense.title,
+                style: Theme.of(context).textTheme.titleLarge,
+            ),
+
             SizedBox(height: 4),
             Row(
               children: [
--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -11,6 +11,25 @@ void main(){
         appBarTheme: AppBarTheme(
           backgroundColor: kcolorScheme.onPrimaryContainer,
           foregroundColor: kcolorScheme.primaryContainer
+        ),
+        cardTheme: const CardThemeData().copyWith(
+            color: kcolorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          )
+          ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kcolorScheme.primaryContainer
+          )
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: kcolorScheme.onSecondaryContainer,
+            fontSize: 18
+          )
         )
       ),
       home: Expenses(), //Value,
