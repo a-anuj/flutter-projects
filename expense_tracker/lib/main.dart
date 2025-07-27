4c302a743f0e507e0cd5f2c4ae843d0caa751e72
a-anuj <anujamirthalingam5@gmail.com>
2025-07-27 16:02:33 +0530

    add colorscheme

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/expenses.dart';
 
+var kcolorScheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+
 void main(){
   runApp(
     MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kcolorScheme,
+        appBarTheme: AppBarTheme(
+          backgroundColor: kcolorScheme.onPrimaryContainer,
+          foregroundColor: kcolorScheme.primaryContainer
+        )
+      ),
       home: Expenses(), //Value,
     ),
   );
