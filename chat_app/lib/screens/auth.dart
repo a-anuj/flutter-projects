07b298f332dbaaa6cddfb411d75571969a86f4fe
a-anuj <anujamirthalingam5@gmail.com>
2025-07-29 21:38:01 +0530

    add logic to connect to firebase for signup

--- chat_app/lib/screens/auth.dart
+++ chat_app/lib/screens/auth.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
+final _firebase = FirebaseAuth.instance;
 class AuthScreen extends StatefulWidget{
   const AuthScreen({super.key});
 
@@ -16,15 +18,28 @@ class _AuthScreenState extends State<AuthScreen>{
   var _enteredEmail = "";
   var _enteredPassword = "";
 
-  void submit(){
+  void submit() async {
     final isValid = _form.currentState!.validate();
-    if(isValid){
-      _form.currentState!.save();
-      print(_enteredPassword);
-      print(_enteredEmail);
+    if(!isValid){
+      return;
+    }
+    _form.currentState!.save();
+
+    if(_isLogin){
+      //
+    } else {
+      try{
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+            email: _enteredEmail,
+            password: _enteredPassword
+        );
+      } on FirebaseAuthException catch (error){
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(error.message ?? "Authentication Failed")));
+      }
+
     }
   }
-
   
   @override
   Widget build(BuildContext context) {
