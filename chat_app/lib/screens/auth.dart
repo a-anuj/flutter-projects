5f822473fd0fa4ae3df68694ec81e5f8759aef5d
a-anuj <anujamirthalingam5@gmail.com>
2025-07-29 09:19:30 +0530

    add form validation

--- chat_app/lib/screens/auth.dart
+++ chat_app/lib/screens/auth.dart
@@ -10,7 +10,21 @@ class AuthScreen extends StatefulWidget{
 }
 
 class _AuthScreenState extends State<AuthScreen>{
+
+  final _form = GlobalKey<FormState>();
   var _isLogin = true;
+  var _enteredEmail = "";
+  var _enteredPassword = "";
+
+  void submit(){
+    final isValid = _form.currentState!.validate();
+    if(isValid){
+      _form.currentState!.save();
+      print(_enteredPassword);
+      print(_enteredEmail);
+    }
+  }
+
   
   @override
   Widget build(BuildContext context) {
@@ -37,6 +51,7 @@ class _AuthScreenState extends State<AuthScreen>{
                   child: Padding(
                       padding: const EdgeInsets.all(16),
                       child: Form(
+                          key: _form,
                           child: Column(
                             mainAxisSize: MainAxisSize.min,
                             children: [
@@ -47,16 +62,34 @@ class _AuthScreenState extends State<AuthScreen>{
                                 keyboardType: TextInputType.emailAddress,
                                 autocorrect: false,
                                 textCapitalization: TextCapitalization.none,
+                                validator: (value){
+                                  if(value==null || value.trim().isEmpty || !value.contains("@")){
+                                    return "Please enter valid email address.";
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value){
+                                  _enteredEmail = value!;
+                                },
                               ),
                               TextFormField(
                                 decoration: InputDecoration(
                                   labelText: "Password"
                                 ),
                                 obscureText: true,
+                                validator: (value){
+                                  if(value==null || value.trim().length<6){
+                                    return "Password must be greater than 6 characters.";
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value){
+                                  _enteredPassword = value!;
+                                },
                               ),
                               const SizedBox(height: 12,),
                               ElevatedButton(
-                                  onPressed: (){},
+                                  onPressed: submit,
                                   style: ElevatedButton.styleFrom(
                                     backgroundColor: Theme.of(context).colorScheme.primaryContainer
                                   ),
