cab1d5dd28e86210b5ac7410da5b626e7b9e39d8
a-anuj <anujamirthalingam5@gmail.com>
2025-07-29 07:56:58 +0530

    instantiated a stateful widget

--- chat_app/lib/screens/auth.dart
+++ chat_app/lib/screens/auth.dart
@@ -1,1 +1,18 @@
-class AuthScreen extends Sta
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget{
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() {
+    return _AuthScreenState();
+  }
+}
+
+class _AuthScreenState extends State<AuthScreen>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
