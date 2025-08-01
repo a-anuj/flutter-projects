a1a1a74266d48e1c3c10c44a42dba6715294f303
a-anuj <anujamirthalingam5@gmail.com>
2025-08-01 17:30:22 +0530

    recieve the image file

--- chat_app/lib/screens/auth.dart
+++ chat_app/lib/screens/auth.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:chat_app/widgets/user_image_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -18,12 +20,14 @@ class _AuthScreenState extends State<AuthScreen>{
   var _isLogin = true;
   var _enteredEmail = "";
   var _enteredPassword = "";
+  File? _selectedImage;
 
   void submit() async {
     final isValid = _form.currentState!.validate();
-    if (!isValid) {
+    if (!isValid || !_isLogin && _selectedImage == null) {
       return;
     }
+
     _form.currentState!.save();
 
     try {
@@ -75,7 +79,9 @@ class _AuthScreenState extends State<AuthScreen>{
                           child: Column(
                             mainAxisSize: MainAxisSize.min,
                             children: [
-                              if (!_isLogin) UserImagePicker(),
+                              if (!_isLogin) UserImagePicker(onPickImage: (pickedImage){
+                                _selectedImage = pickedImage;
+                              },),
                               TextFormField(
                                 decoration: InputDecoration(
                                   labelText: "Email Address"
