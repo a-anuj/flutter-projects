782df60d400854307b5c5de9274fa2392dcb92b9
a-anuj <anujamirthalingam5@gmail.com>
2025-08-02 13:11:17 +0530

    build the apk file

--- chat_app/lib/screens/chat.dart
+++ chat_app/lib/screens/chat.dart
@@ -1,11 +1,34 @@
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app/widgets/chat_messages.dart';
 
-class ChatScreen extends StatelessWidget{
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
-  
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ChatScreenState();
+  }
+
+}
+
+class _ChatScreenState extends State<ChatScreen>{
+
+  void setupPushNotifications() async{
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    final token = await fcm.getToken();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    final fcm = FirebaseMessaging.instance;
+    setupPushNotifications();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
