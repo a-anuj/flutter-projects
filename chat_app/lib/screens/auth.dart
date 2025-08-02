0bc2f3048feb32b25421694108c1a78a5125d863
a-anuj <anujamirthalingam5@gmail.com>
2025-08-02 10:08:33 +0530

    add image,username(dummy for now),email to the firestore

--- chat_app/lib/screens/auth.dart
+++ chat_app/lib/screens/auth.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:chat_app/widgets/user_image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -52,7 +53,14 @@ class _AuthScreenState extends State<AuthScreen>{
             .child("${userCredential.user!.uid}.jpg");
         await storageRef.putFile(_selectedImage!);
         final imageUrl = await storageRef.getDownloadURL();
-        print(imageUrl);
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCredential.user!.uid)
+            .set({
+            'username' : 'pass for now',
+            'email' : _enteredEmail,
+            'image_url' : imageUrl,
+        });
       }
     }
     on FirebaseAuthException catch (error) {
